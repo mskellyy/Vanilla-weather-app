a0099466d7ee877ab629f95a177fad1ee9e00c1d
Kelly <125195808+mskellyy@users.noreply.github.com>
2023-02-22 22:19:28 -0700

    adding JS for today date

---
+++ src/app.js
@@ -0,0 +1,43 @@
+// Code for changing date and time
+
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let currentDate = document.querySelector(".dayDate");
+currentDate.innerHTML = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let currentMonth = document.querySelector(".month");
+currentMonth.innerHTML = months[now.getMonth()];
+
+let currentDay = document.querySelector("#date");
+currentDay.innerHTML = now.getDate();
+
+let currentYear = document.querySelector(".year");
+currentYear.innerHTML = now.getFullYear();
+
+let currentTime = document.querySelector(".time");
+currentTime.innerHTML = now.toLocaleTimeString();
